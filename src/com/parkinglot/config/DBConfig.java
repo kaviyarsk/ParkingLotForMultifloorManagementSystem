614bca993c1d8463f3ac0f3a1cfd7106203738da
kaviyarsk <kaviyarajasingh@gmail.com>
2026-07-31 11:13:18 +0530

    Update DBConfig.java

--- src/com/parkinglot/config/DBConfig.java
+++ src/com/parkinglot/config/DBConfig.java
@@ -1,17 +1,15 @@
 package com.parkinglot.config;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConfig {
-    
     private static final String URL = "jdbc:mysql://localhost:3306/parking_lot_db";
     private static final String USER = "root"; 
     private static final String PASSWORD = "root@12A"; 
     private static Connection connection = null;
+    
     public static Connection getConnection() {
-        
         try {
             if (connection == null || connection.isClosed()) {
                 synchronized (DBConfig.class) { 
